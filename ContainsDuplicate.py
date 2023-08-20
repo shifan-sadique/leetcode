579becb4e385b6b9714805cf93a878077ab26bb6
shifan-sadique <67517134+shifan-sadique@users.noreply.github.com>
2023-08-20 12:40:33 +0530

    Update ContainsDuplicate.py
    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,1]
    Output: true
    Example 2:
    
    Input: nums = [1,2,3,4]
    Output: false
    Example 3:
    
    Input: nums = [1,1,1,3,3,4,3,2,4,2]
    Output: true
    
    
    Runtime: 466ms 
    beats 94% pyhton users

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -2,7 +2,7 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashset=set()
         for i in nums:
-            if i in hashset:
+            if i in hashset: 
                 return True
             hashset.add(i)
         return False
